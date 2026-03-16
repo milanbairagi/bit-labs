dc84300a07e74fe60a279b0f5e6c04e1596ddeae
Milan Bairagi <nmilan626@gmail.com>
2026-03-16 22:20:14 +0545

    feat: add web technology lab 3
    PHP program to check for prime numbers

---
+++ 5th Semester/Web Technology II/Lab-3/index.php
@@ -0,0 +1,18 @@
+<!-- 3. Write a PHP program to check prime number. -->
+<?php
+$number = 29;
+
+function isPrime($n) {
+    if ($n < 2) return false;
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) return false;
+    }
+    return true;
+}
+
+if (isPrime($number)) {
+    echo "$number is a Prime number\n";
+} else {
+    echo "$number is not a Prime number\n";
+}
+?>
