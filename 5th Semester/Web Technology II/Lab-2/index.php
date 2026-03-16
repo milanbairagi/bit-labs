157eeb0272567a412b76aa002d0b7f629d3d20c4
Milan Bairagi <nmilan626@gmail.com>
2026-03-16 22:18:09 +0545

    feat: add web technology lab 2
    PHP program to determine if a number is even or odd

---
+++ 5th Semester/Web Technology II/Lab-2/index.php
@@ -0,0 +1,10 @@
+<!-- 2. Write a PHP program to print Even or odd number -->
+<?php
+$number = 47;
+
+if ($number % 2 == 0) {
+    echo "$number is Even\n";
+} else {
+    echo "$number is Odd\n";
+}
+?>
