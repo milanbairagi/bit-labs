7ec8824032e053fde1a21bd1ad536137f4e45948
Milan Bairagi <nmilan626@gmail.com>
2026-04-23 15:26:08 +0545

    feat: implement Euler's Totient Function and GCD calculation in lab-12.cpp

---
+++ 5th Semester/Information Technology/lab-12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std; 
+
+int gcd(int a, int b) { 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+// Euler Totient Function 
+int etf(int n) { 
+    int result = 1;
+    cout << "Numbers coprime to " << n << ": 1 ";
+    for (int i = 2; i < n; i++) 
+        if (gcd(i, n) == 1) {
+            result++; 
+            cout << i << " ";
+        }
+    
+    cout << endl;
+    return result; 
+} 
+
+// Driver Code
+int main() { 
+    int n;
+    
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int result = etf(n);
+    cout << "Euler's Totient Function for " << n << " is " << result << endl;
+    return 0; 
+}
